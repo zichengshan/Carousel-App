886d539dbcc3eb828649950d1b09f1e83a5b0ff9
Zicheng.S <zichengshan@gmail.com>
2022-03-28 20:11:27 -0700

    Show the user which slide they're on

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ let slidePosition = 0
 const totalSlides = slides.length
 const prevEl = document.getElementById("carousel-button-prev")
 const nextEl = document.getElementById("carousel-button-next")
+const positionEl = document.getElementById("text")
 
 prevEl.addEventListener("click", moveToPrevSlide)
 nextEl.addEventListener("click", moveToNextSlide)
 
+positionEl.innerText = `Slide ${slidePosition+1}`
 function moveToPrevSlide(){
     slidePosition--;
     if(slidePosition < 0) {
         slidePosition = totalSlides-1
     }
     hideAllSlides()
+    positionEl.innerText = `Slide ${slidePosition+1}`
     slides[slidePosition].classList.add("carousel-item-visible")
 }
 
@@ -24,6 +27,7 @@ function moveToNextSlide() {
         slidePosition++
     }
     hideAllSlides()
+    positionEl.innerText = `Slide ${slidePosition+1}`
     slides[slidePosition].classList.add("carousel-item-visible")
 }
 
